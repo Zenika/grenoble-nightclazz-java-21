ba7d6f7134d75fe35396a43163d554335b1f96d6
Arthur DREVOT <arthur.drevot@zenika.com>
2023-09-18 09:47:30 +0200

    Remove spring security matchers

--- src/main/java/com/zenika/grenoble/nightclazz/java21/config/SecurityConfig.java
+++ src/main/java/com/zenika/grenoble/nightclazz/java21/config/SecurityConfig.java
@@ -6,16 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(HttpMethod.POST, "/cities").hasRole("ADMIN")
-                        .anyRequest().permitAll()
-                )
+        return http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
